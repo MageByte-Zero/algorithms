2b33506c231986af78f8514661025657b14b12bb
MageByte <zero_develop@163.com>
2020-05-31 12:55:16 +0800

    Feat(算法)：归并排序

---
+++ src/main/java/com/zero/algorithms/linear/sort/MergeSort.java
@@ -0,0 +1,61 @@
+package com.zero.algorithms.linear.sort;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.Arrays;
+
+/**
+ * 归并排序：时间复杂度 O(nlogn),平均时间复杂度 O(nlogn),最好时间复杂度 O(nlogn)，
+ * 空间时间复杂度 O(n).稳定排序算法。
+ */
+public class MergeSort implements ComparisonSort {
+    @Override
+    public int[] sort(int[] sourceArray) {
+        // copy sourceArray，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+        int length = arr.length;
+        // 递归终止条件
+        if (length < 2) {
+            return arr;
+        }
+        // 将大任务拆分子任务
+        int middle = length / 2;
+        int[] left = Arrays.copyOfRange(arr, 0, middle);
+        int[] right = Arrays.copyOfRange(arr, middle, length);
+        // 递归调用，合并子任务
+        return merge(sort(left), sort(right));
+    }
+
+    /**
+     * 合并数据
+     *
+     * @param left  左边序列
+     * @param right 右边序列
+     * @return
+     */
+    private int[] merge(@NotNull int[] left, @NotNull int[] right) {
+        int[] result = new int[left.length + right.length];
+        int i = 0;
+        // 谁小谁在前面
+        while (left.length > 0 && right.length > 0) {
+            if (left[0] <= right[0]) {
+                result[i++] = left[0];
+                left = Arrays.copyOfRange(left, 1, left.length);
+            } else {
+                result[i++] = right[0];
+                right = Arrays.copyOfRange(right, 1, right.length);
+            }
+        }
+        while (left.length > 0) {
+            result[i++] = left[0];
+            left = Arrays.copyOfRange(left, 1, left.length);
+        }
+
+        while (right.length > 0) {
+            result[i++] = right[0];
+            right = Arrays.copyOfRange(right, 1, right.length);
+        }
+
+        return result;
+    }
+}
--- src/test/java/com/zero/sort/ComparisonSortTest.java
+++ src/test/java/com/zero/sort/ComparisonSortTest.java
@@ -2,7 +2,9 @@ package com.zero.sort;
 
 import com.zero.algorithms.linear.sort.BubbleSort;
 import com.zero.algorithms.linear.sort.InsertionSort;
+import com.zero.algorithms.linear.sort.MergeSort;
 import com.zero.algorithms.linear.sort.SelectionSort;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,11 +13,17 @@ import java.util.Arrays;
 @DisplayName("比较排序算法测试")
 public class ComparisonSortTest {
 
+    private int[] array;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.array = new int[]{3, 5, 4, 2, 1, 6, 5, 2};
+    }
+
     @DisplayName("冒泡排序")
     @Test
     public void testBubbleSort() {
         BubbleSort bubbleSort = new BubbleSort();
-        int[] array = {3, 5, 4, 2, 1, 6};
         int[] sort = bubbleSort.sort(array);
         System.out.println(Arrays.toString(sort));
 
@@ -25,7 +33,6 @@ public class ComparisonSortTest {
     @Test
     public void testInsertSort() {
         InsertionSort insertionSort = new InsertionSort();
-        int[] array = {3, 5, 4, 2, 1, 6, 5, 2};
         int[] sort = insertionSort.sort(array);
         System.out.println(Arrays.toString(sort));
     }
@@ -34,8 +41,15 @@ public class ComparisonSortTest {
     @Test
     public void testSelectSort() {
         SelectionSort selectionSort = new SelectionSort();
-        int[] array = {3, 5, 4, 2, 1, 6, 5};
         int[] sort = selectionSort.sort(array);
+        System.out.println(Arrays.toString(sort));
+    }
+
+    @DisplayName("归并排序")
+    @Test
+    public void testMergeSort() {
+        MergeSort mergeSort = new MergeSort();
+        int[] sort = mergeSort.sort(array);
         System.out.println(Arrays.toString(sort));
     }
 }
