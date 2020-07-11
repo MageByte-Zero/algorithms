f6648e27433e6969d09af1f25fb081cb476bad99
MageByte <zero_develop@163.com>
2020-07-11 15:07:11 +0800

    feat(算法)：优化计数排序空间

--- src/main/java/com/zero/algorithms/linear/sort/CountingSort.java
+++ src/main/java/com/zero/algorithms/linear/sort/CountingSort.java
@@ -9,19 +9,22 @@ public class CountingSort {
         if (sourceArray == null || sourceArray.length <= 1) {
             return new int[0];
         }
-        // 1.查找数列最大值
+        // 1.查找数列最大值,最小值
         int max = sourceArray[0];
+        int min = sourceArray[0];
         for (int value : sourceArray) {
             max = Math.max(max, value);
+            min = Math.min(min, value);
         }
+        int d = max - min;
         // 2.根据数据最大值确定统计数组长度
-        int[] countArray = new int[max + 1];
+        int[] countArray = new int[d + 1];
         // 3. 遍历原始数组映射到统计数组中,统计元素的个数
         for (int value : sourceArray) {
-            countArray[value]++;
+            countArray[value - min]++;
         }
         // 4.统计数组变形，后面的元素等于前面元素之和。目的是定位在结果数组中的排位
-        for (int i = 1; i <= max; i++) {
+        for (int i = 1; i < countArray.length; i++) {
             countArray[i] += countArray[i - 1];
         }
         // 5.倒序遍历原始数组，从统计数组查找对应的正确位置，输出到结果表
@@ -29,9 +32,9 @@ public class CountingSort {
         for (int i = sourceArray.length - 1; i >= 0; i--) {
             int value = sourceArray[i];
             // 分数在 countArray 中的排名, - 1 则是结果数组的下标
-            int index = countArray[value] - 1;
+            int index = countArray[value - min] - 1;
             sortedArray[index] = value;
-            countArray[value]--;
+            countArray[value - min]--;
         }
         return sortedArray;
     }
